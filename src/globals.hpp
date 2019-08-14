5eebf42b819474fda7b63644298683a61e89ea42
Sean D'Souza <sdammobubbles@gmail.com>
2019-08-14 13:16:16 -0400

    Add (non) holonomic singletons and robot abc

---
+++ src/globals.hpp
@@ -0,0 +1,13 @@
+#ifndef GLOBALS_H_
+#define GLOBALS_H_
+
+#define MTR_PORT_LFRONT 1
+#define MTR_PORT_RFRONT 2
+#define MTR_PORT_LBACK 3
+#define MTR_PORT_RBACK 4
+#define MTR_PORT_UTIL1 5
+#define MTR_PORT_UTIL2 6
+#define MTR_PORT_UTIL3 7
+#define MTR_PORT_UTIL4 8
+
+#endif
---
+++ src/nonholonomic_robot.cpp
@@ -0,0 +1,49 @@
+/**
+ * \file nonholonomic_robot.cpp
+ *
+ * This file defines all devices present on the robot.
+ */
+#include "nonholonomic_robot.hpp"
+#include "globals.hpp"
+#include "json.hpp"
+#include "main.h"
+
+NonHolonomicRobot::NonHolonomicRobot()
+    : controller{CONTROLLER_MASTER},
+      partner{CONTROLLER_PARTNER},
+      mtr_lfront{MTR_PORT_LFRONT},
+      mtr_rfront{MTR_PORT_RFRONT},
+      mtr_lback{MTR_PORT_LBACK},
+      mtr_rback{MTR_PORT_RBACK},
+      mtr_util1{MTR_PORT_UTIL1},
+      mtr_util2{MTR_PORT_UTIL2},
+      mtr_util3{MTR_PORT_UTIL3},
+      mtr_util4{MTR_PORT_UTIL4},
+      driving_style{DrivingStyle::SPLIT_ARCADE_DRIVING},
+      drive_type{DriveType::TANK_DRIVE_MTR_4} {}
+
+NonHolonomicRobot::~NonHolonomicRobot() {}
+
+/**
+ * Get instance of Robot singleton
+ */
+NonHolonomicRobot& NonHolonomicRobot::get_robot() {
+	static NonHolonomicRobot instance;
+	// volatile
+	return instance;
+}
+
+/**
+ * Begin robot tasks
+ */
+void NonHolonomicRobot::begin_tasks() {}
+
+/**
+ * Drive robot with controller
+ */
+void NonHolonomicRobot::drive() {}
+
+/**
+ * Stop all motors
+ */
+void NonHolonomicRobot::stop() {}
